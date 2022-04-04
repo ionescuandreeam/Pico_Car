2aab75fb56c8d90ffdf1cf4b5fb56116227fb6d5
ionescuandreeam <95469686+ionescuandreeam@users.noreply.github.com>
2022-04-04 11:31:36 +0300

    added gpio configuration

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+
+#ifdef 
+
+    gpio_init(NR PINLUI)
+    gpio_set_dir(directie)
+    gpio_put(NR PINULUI, HIGH SAU LOW)
+
+#endif
+
 int main(void)
 {  
     stdio_init_all();
-    printf("\n\nHello World Hello\n");
+ 
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
     while (true)
     {
         gpio_put(PICO_DEFAULT_LED_PIN, 1);
